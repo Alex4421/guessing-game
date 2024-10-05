5393e57efe65e14ca8f22d4826ea4eb3f058acd7
Alex4421 <cherevko.alex96@gmail.com>
2024-10-05 12:13:10 +0300

    feat: complete

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,21 +1,41 @@
-class GuessingGame {
-    constructor() {}
-
-    setRange(min, max) {
-
+module.exports = class GuessingGame {
+    constructor(max) {
+      this.min = 0;
+      this.max = max;
     }
-
+  
     guess() {
-
+      if (this.max < this.min) {
+        throw new Error("Неверный диапазон");
+      }
+      return Math.floor((this.min + this.max) / 2);
     }
-
-    lower() {
-
+  
+    lower(guess) {
+      if (guess < this.min || guess > this.max) {
+        throw new Error("Угадывание вне диапазона");
+      }
+      this.max = guess - 1;
     }
-
-    greater() {
-
+  
+    greater(guess) {
+      if (guess < this.min || guess > this.max) {
+        throw new Error("Угадывание вне диапазона");
+      }
+      this.min = guess + 1;
     }
-}
-
-module.exports = GuessingGame;
+  }
+  
+  const game = new (require('./GuessingGame'))(6118);
+  while (true) {
+    const guess = game.guess();
+    console.log(`Угадывание: ${guess}`);
+    if (guess === 998) {
+      break;
+    } else if (guess < 998) {
+      game.greater(guess);
+    } else {
+      game.lower(guess);
+    }
+  }
+  
